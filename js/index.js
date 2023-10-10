49f178867c0e01f9d34ec45017202c430e55f898
DeForestt <deforesttthompson@gmail.com>
2023-10-10 10:06:27 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,62 @@
+let taskList = [];
+
+const getTodoList = async () => {
+    return await fetch("http://localhost:8080/api/tasks")
+}
+
+const deleteTodoList = async (id) => {
+    return await fetch(`http://localhost:8080/api/tasks?id=${id}`, {
+        method: "DELETE"
+    })
+}
+
+const fillList = async () => {
+    const todoList = await getTodoList();
+    const todoListJson = await todoList.json();
+    const list = document.getElementById("list");
+    
+    // fill the taskList with the 'tasks' from the todoListJson
+    taskList = todoListJson.tasks;
+
+    console.log(taskList);
+    list.innerHTML = ""
+    taskList.forEach(task => {
+        const li = document.createElement("li")
+        const text = document.createElement("span")
+        text.innerText = `\t${task.name}`
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "Delete"
+        deleteButton.addEventListener("click", async () => {
+            await deleteTodoList(task.id)
+            fillList()
+        });
+        li.appendChild(deleteButton);
+        li.appendChild(text);
+        list.appendChild(li);
+    });
+}
+
+const postTodoList = async (task) => {
+    return await fetch("http://localhost:8080/api/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(task)
+    })
+}
+
+const addNew = async () => {
+    const name = document.getElementById("name").value;
+    if (name === "") {
+        alert("Please enter a task name");
+        return;
+    }
+    const task = {
+        name,
+    }
+    await postTodoList(task);
+    fillList();
+};
+
+fillList()
